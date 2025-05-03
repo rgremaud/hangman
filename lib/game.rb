5a862b147965d88f522f46a6a4a5378aa1a4724f
Romain Gremaud <romaingremaud@mac.attlocal.net>
2025-05-02 22:11:51 -0400

    Add initial logic for checking letters

--- lib/game.rb
+++ lib/game.rb
@@ -1,12 +1,26 @@
 class Game
-
   def initialize
-  @player = Player.new("Romain")
-  @game = Logic.new
+    @player = Player.new('Romain')
+    @game = Logic.new
   end
 
   def game_loop
     @game.dictionary
+    @game.array_build
+    @game.letter_input
   end
 
+  # including YAML functions as a baseline - Need to update
+  def to_yaml
+    YAML.dump({
+                word: @word,
+                word_array: @word_array
+              })
+  end
+
+  def self.from_yaml(string)
+    data = YAML.load string
+    p data
+    new(data[:word], data[:word_array])
+  end
 end
--- lib/logic.rb
+++ lib/logic.rb
@@ -1,37 +1,56 @@
+# frozen_string_literal: true
+
 require 'yaml'
 
 class Logic
   attr_accessor :word, :word_array
+
   def initialize
     @word = nil
     @word_array = []
-  end
-
-  def to_yaml
-    YAML.dump ({
-      :word => @word,
-      :word_array => @word_array
-    })
-  end
-
-  def self.from_yaml(string)
-    data = YAML.load string
-    p data 
-    self.new(data[:word],data[:word_array])
+    @code_array = []
   end
 
   def dictionary
-    f = File.open("lib/dictionary.txt")
+    f = File.open('lib/dictionary.txt')
     dictionary = []
-    while line = f.gets do
+    while line = f.gets
       dictionary << line
     end
-    dictionary.select! { |word| word.length >= 5 && word.length <= 12}
+    dictionary.select! { |word| word.length >= 6 && word.length <= 13 }
     @word = dictionary.sample
     @word = @word.chomp
     p @word.length
     puts "The word is #{@word}"
   end
-  
- 
+
+  def array_build
+    @word_array = @word.split('')
+    p @word_array
+    p @word_array[0]
+    @word_array.each do |letter|
+      @code_array << '_'
+    end
+    p @code_array
+  end
+
+  def check(letter)
+    if @word_array.include?(letter)
+      # need to update for multiple occurances
+      # Example: @word_array.each_index.select { |letter| @word_array[letter] == letter }
+      puts 'Included'
+      letter_index = @word_array.find_index(letter)
+      @code_array[letter_index] = letter
+      p @code_array
+    else
+      puts 'Not included'
+    end
+  end
+
+  def letter_input
+    print 'Please enter a letter: '
+    letter = gets.to_s.chomp
+    puts letter
+    check(letter)
+  end
 end
